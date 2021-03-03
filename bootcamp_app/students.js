f7d1d23cfeb2f54987bedfa5880c5106e3503cf3
Adam Mohammed <13.adamm@gmail.com>
2021-03-03 17:51:01 -0500

    added parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,6 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
+
+/*
 pool.query(`
   SELECT id, name, cohort_id
   FROM students
@@ -29,3 +47,4 @@ LIMIT ${process.argv[3] || 5};
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
 }).catch(err => console.error('query error', err.stack));
+*/
